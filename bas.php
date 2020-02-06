cefe9378fdb8af427b09cb30aa88bae2e3e8d7c1
Clo3pro <60750089+Clo3pro@users.noreply.github.com>
2020-02-06 20:31:26 +0100

    Delete bas.php

--- bas.php
+++
@@ -1,8 +0,0 @@
-    <div id="Bienvenue-Bas">
-        <div id="Bienvenue" class="container">
-            <div class="title">
-                <h2>Bienvenue Chez PassThèque</h2>
-            </div>
-            <p>Chez <strong>PassThèque</strong>, vous trouverez des livres de tout types avec auxquels vous pourrez accéder à travers divers abonnement que nous vous proposons. </p>
-        </div>
-    </div>
