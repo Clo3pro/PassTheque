9c1718f8c244f1194393665dbd07b9c4b22dbb32
Cloé Berthelin <cloeberthelin1996@gmail.com>
2020-02-08 18:18:50 +0100

    ajout dossier SQL

---
+++ SQL/new-script.sql
@@ -0,0 +1,325 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Feb 05, 2020 at 02:56 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `bibliotheque_script`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Auteur`
+--
+
+CREATE TABLE `Auteur` (
+  `idPersonne` int(11) NOT NULL,
+  `idLivre` varchar(15) NOT NULL,
+  `idRole` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Auteur`
+--
+
+INSERT INTO `Auteur` (`idPersonne`, `idLivre`, `idRole`) VALUES
+(1, '2264069112', 1),
+(3, '2264069112', 2),
+(4, '2264069112', 3),
+(1, '2264056002', 1),
+(1, '2264056169', 1),
+(6, '203585573X', 1),
+(5, '208127857X', 1),
+(5, '2253163503', 1),
+(6, '2253041475', 1),
+(8, '2253160466', 1),
+(8, '2253038741', 1),
+(8, '2253037923', 1),
+(2, '2035867916', 1),
+(9, '2070373096', 1),
+(10, '2081219972', 1),
+(11, '2266152182', 1),
+(12, '2266152182', 3),
+(13, '2809710562', 1),
+(14, '2809710562', 3),
+(15, '2809710562', 3),
+(16, '2266203533', 4),
+(17, '096573840X', 1),
+(18, '9782747014403', 1),
+(18, '9782747014557', 1),
+(18, '9782747014564', 1),
+(18, '9782747028554', 1),
+(19, '2070518426',1),
+(19,'2070524558',1),
+(19,'2070528189',1),
+(19,'2070543587',1),
+(19,'2070556859',1),
+(19,'2070572676',1),
+(19,'9782070615360',1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Editeur`
+--
+
+CREATE TABLE `Editeur` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Editeur`
+--
+
+INSERT INTO `Editeur` (`id`, `libelle`) VALUES
+(1, 'Belfond'),
+(2, 'Flammarion'),
+(3, 'Librio'),
+(4, 'Pocket'),
+(5, 'Larousse'),
+(6, 'Le livre de poche'),
+(7, 'Folio Théâtre'),
+(8, 'Philippe Picquier'),
+(9, 'Guardian'),
+(10, 'Bayard Jeunesse'),
+(11, 'Gallimard Jeunesse');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Genre`
+--
+
+CREATE TABLE `Genre` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Genre`
+--
+
+INSERT INTO `Genre` (`id`, `libelle`) VALUES
+(8, 'Manga'),
+(6, 'Bande dessinée'),
+(4, 'Essai'),
+(7, 'Fantasy'),
+(3, 'Nouvelle'),
+(5, 'Poésie'),
+(2, 'Roman'),
+(1, 'Théâtre');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Langue`
+--
+
+CREATE TABLE `Langue` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Langue`
+--
+
+INSERT INTO `Langue` (`id`, `libelle`) VALUES
+(1, 'Anglais'),
+(2, 'Français'),
+(3, 'Japonais'),
+(4, 'Espagnol');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Livre`
+--
+
+CREATE TABLE `Livre` (
+  `isbn` varchar(15) NOT NULL,
+  `titre` varchar(500) NOT NULL,
+  `editeur` int(11) NOT NULL,
+  `annee` int(11) DEFAULT NULL,
+  `genre` int(11) DEFAULT NULL,
+  `langue` int(11) DEFAULT NULL,
+  `nbpages` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Livre`
+--
+
+INSERT INTO `Livre` (`isbn`, `titre`, `editeur`, `annee`, `genre`, `langue`, `nbpages`) VALUES
+('096573840X', 'A short history of Nearly Everything', 9, 2003, 4, 1, NULL),
+('203585573X', 'Ruy Blas', 2, 1838, 1, 2, 270),
+('2035867916', 'L\'illusion comique', 6, 1639, 1, 2, NULL),
+('2070373096', 'Les Paravents', 7, 1961, 1, 2, NULL),
+('2081219972', 'Le Comédien désincarné', 2, 2009, 2, 2, 390),
+('208127857X', 'Un fil à la patte', 7, 1894, 1, 2, 208),
+('2253037923', 'Le misanthrope', 6, 1666, 1, 2, NULL),
+('2253038741', 'Les femmes savantes', 6, 1672, 1, 2, NULL),
+('2253040156', 'Poètes français des XIXe et XXe sciècles', 6, 2009, 5, 2, NULL),
+('2253041475', 'Hernani', 6, 1830, 1, 2, NULL),
+('2253160466', 'Les précieuses ridicules', 6, 1660, 1, 2, 355),
+('2253163503', 'Le Dindon', 6, 1989, 1, 2, 107),
+('2264056002', 'La ballade de l\'impossible', 1, 2002, 2, 2, 456),
+('2264056169', 'Kafka sur le rivage', 1, 2002, 2, 2, 648),
+('2264069112', 'L\'étrange bibliothèque', 1, 2015, 3, 2, 80),
+('2266152181', 'Le cid', 2, 1637, 1, 2, 208),
+('2266152182', 'L\'art de la guerre', 2, 1963, 4, 2, NULL),
+('2266203533', 'Les Contemplations', 4, 1856, 5, 2, 672),
+('2809710562', 'La tombe des lucioles', 8, 1967, 2, 2, 143),
+('9782747014403', 'Eragon', 10, 2003, 7, 2, 694),
+('9782747014557', 'L\'Aîné', 10, 2006, 7, 2, 805),
+('9782747014564', 'Brisingr', 10, 2009, 7, 2, 825),
+('9782747028554', 'L\'Héritage', 10, 2012, 7, 2, 900),
+('2070518426', 'Harry Potter à l\'école des sorciers',11,1998,7,2,308),
+('2070524558', 'Harry Potter et la Chambre des Secrets',11,1999,7,2,364),
+('2070528189', 'Harry Potter et le Prisonnier d\'Azkaban',11,1999,7,2,474),
+('2070543587', 'Harry Potter et la Coupe de Feu',11,2000,7,2,656),
+('2070556859', 'Harry Potter et l\'Ordre du Phoenix',11,2003,7,2,984),
+('2070572676', 'Harry Potter et le Prince de Sang-mêlé',11,2005,7,2,720),
+('9782070615360', 'Harry Potter et les Reliques de la Mort',11,2007,7,2,816);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Personne`
+--
+
+CREATE TABLE `Personne` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(150) NOT NULL,
+  `prenom` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Personne`
+--
+
+INSERT INTO `Personne` (`id`, `nom`, `prenom`) VALUES
+(1, 'Murakami', 'Haruki'),
+(2, 'Corneille', 'Pierre'),
+(3, 'Menschik', 'Kat'),
+(4, 'Morita', 'Helene'),
+(5, 'Feydeau', 'Georges'),
+(6, 'Hugo', 'Victor'),
+(7, 'Chedeville', 'Elise'),
+(8, 'Molière', NULL),
+(9, 'Genet', 'Jean'),
+(10, 'Jouvet', 'Louis'),
+(11, 'Tzu', 'Sun'),
+(12, 'Amiot', 'Joseph-Marie'),
+(13, 'Nosaka', 'Akiyuki'),
+(14, 'De Vos', 'Patrick'),
+(15, 'Gossot', 'Anne'),
+(16, 'Chamarat-Malandain', 'Gabrielle'),
+(17, 'Bryson', 'Bill'),
+(18, 'Paolini', 'Christopher'),
+(19, 'Rowling','J.K');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Role`
+--
+
+CREATE TABLE `Role` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Role`
+--
+
+INSERT INTO `Role` (`id`, `libelle`) VALUES
+(1, 'Ecrivain'),
+(2, 'Illustrateur'),
+(3, 'Traducteur'),
+(4, 'Préface');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Editeur`
+--
+ALTER TABLE `Editeur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Genre`
+--
+ALTER TABLE `Genre`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `libelle` (`libelle`);
+
+--
+-- Indexes for table `Langue`
+--
+ALTER TABLE `Langue`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Livre`
+--
+ALTER TABLE `Livre`
+  ADD PRIMARY KEY (`isbn`);
+
+--
+-- Indexes for table `Personne`
+--
+ALTER TABLE `Personne`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Role`
+--
+ALTER TABLE `Role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Editeur`
+--
+ALTER TABLE `Editeur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `Genre`
+--
+ALTER TABLE `Genre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `Langue`
+--
+ALTER TABLE `Langue`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `Personne`
+--
+ALTER TABLE `Personne`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `Role`
+--
+ALTER TABLE `Role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
